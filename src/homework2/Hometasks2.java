9735d94d253fc6e1117498c00ee390622773970c
Geo <geornazarov@hotmail.com>
2023-10-03 23:22:08 +0300

    Homework already done

---
+++ src/homework2/Hometasks2.java
@@ -0,0 +1,32 @@
+package homework2;
+
+public class Hometasks2 {
+
+    public static void main(String[] args) {
+
+        int totalMailAmount, sendMailAmount;
+        totalMailAmount = 724;
+        sendMailAmount = 236;
+        System.out.println(totalMailAmount);
+        System.out.println(sendMailAmount);
+        System.out.println("Draft mail: 10");
+
+        double time = 12, distance = 8661.6, speed;
+        speed = distance / time / 3.6;
+        System.out.println(speed);
+
+        int number = 73, res;
+        res = 7 + 3;
+        System.out.println(res);
+
+        int a = 8, b = 12;
+
+        a = b;
+        b = a - 4;
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+
+}
